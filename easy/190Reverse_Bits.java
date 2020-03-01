a9484c66ebc60556d94a8902d0fbe15dd0a52948
Pandara320 <31490140+Pandara320@users.noreply.github.com>
2020-02-29 21:14:59 -0500

    Update 190Reverse_Bits.java

--- easy/190Reverse_Bits.java
+++ easy/190Reverse_Bits.java
@@ -1,17 +1,16 @@
 public class Solution {
-    // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
         int result = 0;
-        int count = 0;
-        for ( int i =0; i<32; i++){
-            result = result << 1;
-                if((n & 0x1) == 1){
-                    result | = 0x1;
-                    count = count + 1;
-                }
+        for (int i = 0; i< 32; i++)
+        {
+            result = result <<1;
+            if((n & 1) ==1)
+            {
+                result |= 1;
+            }
             n = n>>1;
         }
-        return count;
-        
+        return result;
     }
 }
