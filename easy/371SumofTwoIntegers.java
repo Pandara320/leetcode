590b2e7ce86c6e9b8ce46930412b67c88e36e859
Pandara320 <31490140+Pandara320@users.noreply.github.com>
2020-03-05 15:46:12 -0500

    Update 371SumofTwoIntegers.java

--- easy/371SumofTwoIntegers.java
+++ easy/371SumofTwoIntegers.java
@@ -1,10 +1,11 @@
 class Solution {
     public int getSum(int a, int b) {
         // carry now contains common set bits of x and y
-        int  carry = a & b;
-        a = a ^ b;
-        b = carry << 1;
+        while(b!=0){
+            int  carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
         return a;
-
     }
 }
